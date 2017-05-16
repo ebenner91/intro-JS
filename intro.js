d323d532f8479d54c7cc408257b551343fbbcf69
Elizabeth Benner <ebenner@mail.greenriver.edu>
2017-05-16 13:58:13 -0500

    Completed Step 2 of exercise

--- intro.js
+++ intro.js
@@ -1,1 +1,26 @@
 console.log("Introduction to JavaScript");
+
+FizzBuzz();
+
+function FizzBuzz()
+{
+    for(var i = 1; i <= 100; i++)
+    {
+        if(i % 3 === 0 && i % 5 === 0)
+        {
+            console.log("FizzBuzz");
+        }
+        else if(i % 3 === 0 )
+        {
+            console.log("Fizz");
+        }
+        else if(i % 5 === 0)
+        {
+            console.log("Buzz");
+        }
+        else
+        {
+            console.log(i);
+        }
+    }
+}
